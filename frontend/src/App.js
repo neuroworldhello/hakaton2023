205c2475c5b6eb2140cd1202d870597ddcc420c1
egor.krupnyi <egor.krupnyi@iac.spb.ru>
2023-05-04 13:05:20 +0300

    add material

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import './dist/output.css';
+import BestPractices from "./components/BestPractices";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="  w-160 underline bg-button">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BestPractices/>
   );
 }
 
