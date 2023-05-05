5551fb36cddfe0794ef5b589534d09c348d0ae74
Роман <r.nikitin@iac.spb.ru>
2023-05-05 15:20:32 +0300

    Add commenting feature

---
+++ backend/src/main/java/com/example/backend/configuration/MapperConfig.java
@@ -0,0 +1,11 @@
+package com.example.backend.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
