965de0b3e15da94bf8f0356fd6db2bfc96471848
Ariona <e.petrushenko@inbox.ru>
2023-05-05 14:08:33 +0300

    работа над диалогом комментариев

--- frontend/src/components/CommentsDialog.js
+++ frontend/src/components/CommentsDialog.js
@@ -2,9 +2,41 @@ import React, {useState} from "react";
 import {Dialog, DialogContent, DialogContentText, DialogTitle, TextField} from "@mui/material";
 import Button from "./Button";
 
+const testData = [
+    {
+        id: 1,
+        text: 'Формирование описания экранов',
+        practice: 'Формирование описания экранов',
+        author: 'Автор 1',
+        createdAt: '2022-05-03'
+    },
+    {
+        id: 2,
+        text: 'На основе OpenAPI Spec документа реализация Spring Boot приложения с использованием требуемых технологий',
+        practice: '',
+        author: 'Автор 2',
+        createdAt: '2022-05-03'
+    },
+    {id: 3, text: 'Code review', practice: 'Формирование описания экранов', author: 'Автор 3', createdAt: '2022-05-03'},
+    {
+        id: 4,
+        text: 'Составление тест-кейсов на основе описания технических решений',
+        practice: 'Формирование описания экранов',
+        author: 'Автор 4',
+        createdAt: '2022-05-03'
+    },
+    {
+        id: 5,
+        text: 'Генерация Ansible-vars файла для конфигурации LDAP из конфигурации существующего LDAP-сервера.',
+        practice: 'Формирование описания экранов',
+        author: 'Автор 5',
+        createdAt: '2022-05-03'
+    },
+]
+
 export function CommentsDialog({dialogOpen, setDialogOpen}) {
     const [comment, setComment] = useState('');
-    const [comments, setComments] = useState([]);
+    const [comments, setComments] = useState(testData);
 
     const handleDialogClose = () => {
         setDialogOpen(false);
@@ -40,16 +72,25 @@ export function CommentsDialog({dialogOpen, setDialogOpen}) {
                         className="w-full"
                     />
                     <div className='py-10 flex justify-end'>
-                            <Button
-                                onClick={handleSaveButtonClick}
-                                disabled={!comment}
-                            >
-                                Добавить
-                            </Button>
+                        <Button
+                            onClick={handleSaveButtonClick}
+                            disabled={!comment}
+                        >
+                            Добавить
+                        </Button>
                     </div>
                     <div>
                         {comments.length > 0 &&
-                            <div>комментарии</div>
+                        <div>
+                            {comments.map(comment =>
+                                <div id={comment.id} className='py-10'>
+                                    <div className='flex flex-row'>
+                                        <div className='pr-6'>{comment.author}</div>
+                                        <div>{comment.createdAt}</div>
+                                    </div>
+                                    {comment.text}
+                                </div>)}
+                        </div>
                         }
                         {comments.length === 0 &&
                         <div className='flex justify-center'>
