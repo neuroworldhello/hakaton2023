0dd1a3b35010f3ab0dc7fab10321cab7969c7e05
Роман <r.nikitin@iac.spb.ru>
2023-05-05 19:06:42 +0300

    Refactor comments

--- backend/src/main/java/com/example/backend/service/CommentService.java
+++ backend/src/main/java/com/example/backend/service/CommentService.java
@@ -5,6 +5,23 @@ import com.example.backend.domain.Comment;
 import java.util.List;
 
 public interface CommentService {
-    List<Comment> getCommentsByPracticeId(Long practiceId);
-    Comment addComment(Long practiceId, String text);
+    /**
+     * Получает список комментариев для практики с указанным идентификатором,
+     * сортированный по дате добавления в порядке убывания.
+     *
+     * @param practiceId идентификатор практики
+     * @return список комментариев
+     */
+    List<Comment> getCommentsForPractice(Long practiceId);
+
+    /**
+     * Добавляет комментарий к практике с указанным идентификатором.
+     * Автор комментария должен быть текущим пользователем, получаемым
+     * через сервис Authorisation.
+     *
+     * @param practiceId идентификатор практики
+     * @param text текст комментария
+     * @return добавленный комментарий
+     */
+    Comment addCommentToPractice(Long practiceId, String text);
 }
--- backend/src/test/java/com/example/backend/service/impl/CommentServiceImplTest.java
+++ backend/src/test/java/com/example/backend/service/impl/CommentServiceImplTest.java
@@ -2,89 +2,126 @@ package com.example.backend.service.impl;
 
 import com.example.backend.domain.*;
 import com.example.backend.repository.*;
-import com.example.backend.service.CommentService;
-import org.assertj.core.api.Assertions;
 import org.junit.Assert;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@SpringBootTest
-public class CommentServiceImplTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
+@WithMockUser(username = "Test Author")
+public class CommentServiceImplTest {
     @Autowired
-    private CommentService commentService;
+    private CommentServiceImpl commentService;
 
     @Autowired
     private PracticeRepository practiceRepository;
 
     @Autowired
     private AuthorRepository authorRepository;
-
-    @Autowired
-    private CategoryRepository categoryRepository;
-
     @Autowired
     private TeamRepository teamRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Autowired
     private CommentRepository commentRepository;
 
     @Test
-    @WithMockUser(username = "testUser")
-    public void testGetCommentsByPracticeId() {
-        Author author = authorRepository.save(Author.builder().name("Author 1").build());
-
-        Category category = Category.builder()
-                .name("Category name")
-                .build();
-
-        Team team = Team.builder()
-                .name("Team name")
-                .build();
-
-        Practice practice = Practice.builder()
-                .name("Practice name")
-                .description("Practice description")
-                .documentLink("http://example.com")
-                .rating(5)
-                .category(category)
-                .author(author)
-                .team(team)
-                .build();
-
+    void getCommentsForPractice() {
+        Author author = createAuthor();
+        // Создаем тестовые данные
+        Practice practice = createPractice();
         Comment comment1 = Comment.builder()
-                .text("Comment 1")
+                .text("Test comment 1")
+                .createdAt(LocalDateTime.of(2023, 5, 4, 10, 0))
                 .author(author)
                 .practice(practice)
-                .createdAt(LocalDateTime.now())
                 .build();
         Comment comment2 = Comment.builder()
-                .text("Comment 2")
+                .text("Test comment 2")
+                .createdAt(LocalDateTime.of(2023, 5, 4, 11, 0))
                 .author(author)
+                .practice(practice)
+                .build();
+        Comment comment3 = Comment.builder()
+                .text("Test comment 3")
+                .createdAt(LocalDateTime.of(2023, 5, 4, 12, 0))
+                .author(author)
+                .practice(practice)
+                .build();
+        commentRepository.saveAll(List.of(comment1, comment2, comment3));
+
+        List<Comment> comments = commentService.getCommentsForPractice(practice.getId());
+        assertThat(comments).hasSize(3);
+        assertThat(comments.get(0).getText()).isEqualTo("Test comment 3");
+        assertThat(comments.get(1).getText()).isEqualTo("Test comment 2");
+        assertThat(comments.get(2).getText()).isEqualTo("Test comment 1");
+    }
+
+    @Test
+    void addCommentToPractice() {
+        // Создаем тестовые данные
+        Practice practice = createPractice();
+
+        String commentText = "Test comment";
+
+        // Выполняем тестируемый метод
+        Comment comment = commentService.addCommentToPractice(practice.getId(), commentText);
+
+        // Проверяем результаты
+        Assert.assertNotNull(comment);
+        Assert.assertEquals(commentText, comment.getText());
+        Assert.assertEquals(practice.getId(), comment.getPractice().getId());
+    }
+
+    private Practice createPractice() {
+        Practice practice = Practice.builder()
+                .name("Test practice")
+                .description("Test description")
+                .documentLink("http://test.com")
+                .rating(4)
+                .author(createAuthor())
+                .category(createCategory())
+                .team(createTeam())
+                .build();
+        return practiceRepository.save(practice);
+    }
+
+    private Author createAuthor() {
+        Author author = Author.builder()
+                .name("Test author")
+                .build();
+        return authorRepository.save(author);
+    }
+
+    private Category createCategory() {
+        Category category = Category.builder()
+                .name("Test category")
+                .build();
+        return categoryRepository.save(category);
+    }
+
+    private Team createTeam() {
+        Team team = Team.builder()
+                .name("Test team")
+                .build();
+        return teamRepository.save(team);
+    }
+
+    private Comment createComment(Practice practice) {
+        Comment comment = Comment.builder()
+                .text("Test comment")
+                .author(createAuthor())
                 .practice(practice)
                 .createdAt(LocalDateTime.now())
                 .build();
-
-        categoryRepository.save(category);
-        authorRepository.save(author);
-        teamRepository.save(team);
-        practiceRepository.save(practice);
-        commentRepository.saveAll(List.of(comment1, comment2));
-
-        // When
-        List<Comment> comments = commentService.getCommentsByPracticeId(practice.getId());
-
-        // Then
-        Assertions.assertThat(comments)
-                .extracting("text")
-                .containsExactlyInAnyOrder("Comment 1", "Comment 2");
+        return commentRepository.save(comment);
     }
 
 }
